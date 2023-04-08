ecab91cd3d54d0c9b2d4a9b634531ca381a1fea4
Spencer Nelson <spencer@b612foundation.org>
2023-04-08 23:29:27 +0200

    Add classmethod for building from a flattened dataframe

---
+++ quiver/schemagraph.py
@@ -0,0 +1,34 @@
+from typing import Optional, Callable
+import pyarrow as pa
+
+
+def _walk_schema(
+    field: pa.Field,
+    visitor: Callable[pa.Field, list[pa.Field]],
+    ancestors: Optional[list] = None,
+):
+    # Visit every struct field embedded within a field in depth-first fashion.
+    #
+    # Runs visitor(struct_field, [ancestors...]) on each struct field.
+    if ancestors is None:
+        ancestors = [field]
+    else:
+        ancestors += [field]
+
+    for subfield in field.type:
+        if pa.types.is_struct(subfield.type):
+            _walk_schema(subfield, visitor, ancestors)
+
+    ancestors.pop()
+    visitor(field, ancestors)
+
+
+def compute_depth(schema: pa.Schema) -> int:
+    """Compute the maximum depth of the schema, thinking of it as a tree which contains struct types."""
+    max_depth = 1
+    for field in schema:
+        if pa.types.is_struct(field.type):
+            depth = 1 + compute_depth(pa.schema(field.type))
+            if depth > max_depth:
+                max_depth = depth
+    return max_depth
--- quiver/tables.py
+++ quiver/tables.py
@@ -9,6 +9,7 @@ import numpy.typing as npt
 from abc import ABC, abstractmethod
 
 from .errors import TableFragmentedError
+from .schemagraph import compute_depth, _walk_schema
 
 _METADATA_MODEL_KEY = b"__quiver_model_pickle"
 _METADATA_NAME_KEY = b"__quiver_model_name"
@@ -32,6 +33,10 @@ class TableMetaclass(type):
             raise TypeError(f"Table {name} schema attribute must be a pyarrow.Schema")
         accessors = dict(cls.generate_accessors(attrs["schema"]))
         attrs.update(accessors)
+
+        # Compute the depth of the schema, which is used when flattening.
+        attrs["_schema_depth"] = compute_depth(attrs["schema"])
+
         return super().__new__(cls, name, bases, attrs)
 
     def generate_accessors(schema: pa.Schema):
@@ -87,6 +92,97 @@ class TableBase(metaclass=TableMetaclass):
         table = pa.Table.from_pydict(d, schema=cls.schema)
         return cls(table=table)
 
+    @classmethod
+    def from_dataframe(cls, df: pd.DataFrame):
+        """Load a DataFrame into the Table.
+
+        If the DataFrame is missing any of the Table's columns, an
+        error is raised. If the DataFrame has extra columns, they are
+        ignored.
+
+        This function cannot load "flattened" dataframes. This only
+        matters for nested Tables which contain other Table
+        definitions as fields. For that use case, either load an
+        unflattened DataFrame, or use from_flat_dataframe.
+        """
+
+        table = pa.Table.from_pandas(df, schema=cls.schema)
+        return cls(table=table)
+
+    @classmethod
+    def from_flat_dataframe(cls, df: pd.DataFrame):
+        """Load a flattened DataFrame into the Table.
+
+        known bug: Doesn't correctly interpret fixed-length lists.
+        """
+        struct_fields = []
+        for field in cls.schema:
+            if pa.types.is_struct(field.type):
+                struct_fields.append(field)
+
+        if len(struct_fields) == 0:
+            return cls.from_dataframe(df, schema=cls.schema)
+
+        root = pa.field("", pa.struct(cls.schema))
+
+        struct_arrays = {}
+
+        def visitor(field: pa.Field, ancestors: list[pa.Field]):
+            nonlocal df
+            if len(ancestors) == 0:
+                # Root - gets special behavior.
+                df_key = ""
+            else:
+                df_key = ".".join([f.name for f in ancestors if f.name] + [field.name])
+
+            # Pull out just the columns relevant to this field
+            field_columns = df.columns[df.columns.str.startswith(df_key)]
+            field_df = df[field_columns]
+
+            # Replace column names like "foo.bar.baz" with "baz"
+            if len(ancestors) == 0:
+                names = field_df.columns
+            else:
+                names = field_df.columns.str.slice(len(df_key) + 1)
+            field_df.columns = names
+
+            # Build a StructArray of all of the children
+            arrays = []
+            for subfield in field.type:
+                sa_key = df_key + "." + subfield.name if df_key else subfield.name
+                if sa_key in struct_arrays:
+                    arrays.append(struct_arrays[sa_key])
+                else:
+                    arrays.append(field_df[subfield.name])
+            sa = pa.StructArray.from_arrays(arrays, fields=list(field.type))
+            struct_arrays[df_key] = sa
+
+            # Clean the fields out
+            df = df.drop(field_columns, axis="columns")
+
+        _walk_schema(root, visitor, None)
+        # Now build a table back up
+        sa = struct_arrays[""]
+
+        table_arrays = []
+        for subfield in cls.schema:
+            table_arrays.append(sa.field(subfield.name))
+            
+        table = pa.Table.from_arrays(table_arrays, schema=cls.schema)
+        return cls(table=table)
+
+
+    def flattened_table(self) -> pa.Table:
+        """Completely flatten the Table's underlying Arrow table,
+        taking into account any nested structure, and return the data
+        table itself.
+
+        """
+        table = self.table
+        for i in range(self._schema_depth - 1):
+            table = table.flatten()
+        return table
+
     def select(self: TTableBase, column_name: str, value: Any) -> TTableBase:
         """Select from the table by exact match, returning a new
         Table which only contains rows for which the value in
@@ -140,6 +236,25 @@ class TableBase(metaclass=TableMetaclass):
             )
         arrays = [chunked_array.chunks[0] for chunked_array in self.table.columns]
         return pa.StructArray.from_arrays(arrays, fields=list(self.schema))
+
+    def to_dataframe(self, flatten: bool = True) -> pd.DataFrame:
+        """Returns self as a pandas DataFrame.
+
+        If flatten is true, then any nested hierarchy is flattened: if
+        the Table's schema contains a struct named "foo" with field
+        "a", "b", and "c", then the resulting DataFrame will include
+        columns "foo.a", "foo.b", "foo.c". This is done fully for any
+        deeply nested structure, for example "foo.bar.baz.c".
+
+        If flatten is false, then that struct will be in a single
+        "foo" column, and the values will of the column will be
+        dictionaries representing the struct values.
+
+        """
+        table = self.table
+        if flatten:
+            table = self.flatten()
+        return table.to_pandas()
 
     @classmethod
     def as_field(
