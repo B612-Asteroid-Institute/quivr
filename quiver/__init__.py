bbcc96a8529b4ed1842ed0a056988775b0a6ba0f
Spencer Nelson <spencer@b612foundation.org>
2023-04-12 09:34:17 -0700

    Add StringIndex to top-level API

--- quiver/__init__.py
+++ quiver/__init__.py
@@ -1,5 +1,12 @@
 from .concat import concatenate
+from .indexing import StringIndex
 from .matrix import MatrixArray, MatrixExtensionType
 from .tables import TableBase
 
-__all__ = ["TableBase", "MatrixArray", "MatrixExtensionType", "concatenate"]
+__all__ = [
+    "TableBase",
+    "MatrixArray",
+    "MatrixExtensionType",
+    "concatenate",
+    "StringIndex",
+]
