e573fe53a821485fc7b5eff2f0e4d60ebfb28061
Spencer Nelson <spencer@b612foundation.org>
2023-06-19 06:44:38 -0700

    Fully type quivr/schemagraph.py

--- quivr/schemagraph.py
+++ quivr/schemagraph.py
@@ -6,8 +6,8 @@ import pyarrow as pa
 def _walk_schema(
     field: pa.Field,
     visitor: Callable[[pa.Field, list[pa.Field]], NoReturn],
-    ancestors: Optional[list] = None,
-):
+    ancestors: Optional[list[pa.Field]] = None,
+) -> None:
     # Visit every struct field embedded within a field in depth-first fashion.
     #
     # Runs visitor(struct_field, [ancestors...]) on each struct field.
